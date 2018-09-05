b69e77fb0cf9e2d2f65ea8997397a7fd262ba16c
Saheel Godhane <saheel.godhane@microsoft.com>
2018-09-05 14:20:20 +0530

    completed module 5 and 6

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -8,7 +8,7 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        RankedGradeBook(string name) : base(name)
+        public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
         }
